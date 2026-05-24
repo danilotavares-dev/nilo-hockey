9104747203da6d3f5da6af8b64d15494c30d45bf
danilotavares-dev <danilotavarespr00@gmail.com>
2026-05-24 19:11:23 -0300

    feat: Puck finished

---
+++ web/src/game/entities/Puck.ts
@@ -0,0 +1,27 @@
+export class Puck {
+    x: number
+    y: number
+    vx: number
+    vy: number
+    radius: number
+
+    constructor (x: number, y: number, radius: number) {
+        this.x = x
+        this.y = y
+        this.vx = 0
+        this.vy = 0
+        this.radius = radius
+    }
+
+    update() {
+        this.x += this.vx
+        this.vy += this.vy
+    }
+
+    reset(centerX: number, centerY: number) {
+        this.x = centerX
+        this.y = centerY
+        this.vx = 0
+        this.vx = 0
+    }
+}
